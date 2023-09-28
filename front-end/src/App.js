a080ba0639f4fac8680ec2aea43d82a0367ebd09
1192807 <nehasarnaik1997@gmail.com>
2023-09-28 18:20:27 +0530

    Initial commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from "./Components/Home/Home"
+import Register from "./Components/Register/Register"
+
 function App() {
   return (
     <div className="App">
+        <BrowserRouter>
         <div className='header background_colour'>
           <h1 className='text_padding'>Product Name</h1>
         </div>
-        <Home></Home>
+        
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- front-end/src/Components/Home/Home.js
+++ front-end/src/Components/Home/Home.js
@@ -1,19 +1,27 @@
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../script.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from '../Register/Register';
+import { Link } from "react-router-dom";
 
-
-const  Home = () => {
+const Home = () => {
     return ( 
         <div>
-            <nav class="navbar navbar-expand-lg  nav">
+             <nav class="navbar navbar-expand-lg  nav background">
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-               
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <Link className="nav-link"></Link>
+                </li>
+                <li class="nav-item">
+                    <Link to={'/register'} class="nav-link padding">Register</Link>
+                </li>
+                <li class="nav-item">
+                    <Link to={'/register'} class="nav-link padding">Login</Link>
+                </li>
+                </ul>
             </div>
             </nav>
-            <div className="background">
-            </div>
-               
-            
         </div>
      );
 }
