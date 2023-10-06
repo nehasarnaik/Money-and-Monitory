fc2aef21a287d2eefc872ff5bf6ec85754f7c02d
1192807 <nehasarnaik1997@gmail.com>
2023-10-06 18:36:59 +0530

    Changed properties file in api-gateway

--- backend/account_service/src/main/java/com/example/moneyandmonitory/account_service/Controller/AccountController.java
+++ backend/account_service/src/main/java/com/example/moneyandmonitory/account_service/Controller/AccountController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 @RequestMapping("/account")
 public class AccountController {
     @Autowired
